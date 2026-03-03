30d1d684de768373e3f3c233c98ab0917fd4d747
gonzalez-7 <20agonzalezp@gmail.com>
2026-03-02 17:48:34 -0800

    Added membership status, last check-in tracking, phone number and address for emergency contact, registration fee, balance due, AutoPay, Join Date and Last check-in date.

--- COMP003A.Final_Project/GymMember.cs
+++ COMP003A.Final_Project/GymMember.cs
@@ -7,12 +7,15 @@ namespace COMP003A.Final_Project
 	internal class GymMember
 	{
 		public string MemberId;
+		public string MembershipStatus;
 		public string FirstName;
 		public string LastName;
 		public string Phone;
 		public string Email;
 		public string Address;
 		public string EmergencyContactName;
+		public string EmergencyContactPhone;
+		public string EmergencyContactAddress;
 
 		public int Age;
 		public int MembershipDurationMonths;
@@ -21,14 +24,20 @@ namespace COMP003A.Final_Project
 
 		public double HeightCm;
 		public double WeightKg;
+		public double RegistrationFeePaid;
 		public double MonthlyFee;
+		public double BalanceDue;
 
 		public bool HasTrainer;
 		public bool HasMedicalCondition;
 		public bool IsActive;
 		public bool IsStudent;
+		public bool AutoPayEnabled;
 
 		public int MembershipTypeCode;
 		public int PaymentPlanCode;
+
+		public DateTime JoinDate;
+		public DateTime LastCheckInDate;
 	}
 }
