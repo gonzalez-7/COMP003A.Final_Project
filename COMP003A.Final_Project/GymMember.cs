992c61d9a005929f7c2a44c9d0c0070afacb8813
gonzalez-7 <20agonzalezp@gmail.com>
2026-03-02 16:39:01 -0800

    Added string fields to GymMember

--- COMP003A.Final_Project/GymMember.cs
+++ COMP003A.Final_Project/GymMember.cs
@@ -6,5 +6,12 @@ namespace COMP003A.Final_Project
 {
 	internal class GymMember
 	{
+		public string MemberId;
+		public string FirstName;
+		public string LastName;
+		public string Phone;
+		public string Email;
+		public string Address;
+		public string EmergencyContactName;
 	}
 }
